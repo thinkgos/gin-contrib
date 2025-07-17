2c20cf8e2cd34e7bf633c4a55e1afce6f859b9fd
thinkgo <thinkgo@aliyun.com>
2025-07-17 08:49:11 +0800

    fix: fix pprof route

--- pprof/pprof.go
+++ pprof/pprof.go
@@ -1,6 +1,7 @@
 package pprof
 
 import (
+	"expvar"
 	"net/http/pprof"
 
 	"github.com/gin-gonic/gin"
@@ -14,25 +15,26 @@ const (
 // Router the standard HandlerFuncs from the net/http/pprof package with
 // the provided gin.Engine. prefixOptions is a optional. If not prefixOptions,
 // the default path prefix("/debug/pprof") is used, otherwise first prefixOptions will be path prefix.
-func Router(router *gin.Engine, prefixOptions ...string) {
+// "/debug/vars" use for expvar
+func Router(g gin.IRouter, prefixOptions ...string) {
 	prefix := defaultPrefix
 	if len(prefixOptions) > 0 {
 		prefix = prefixOptions[0]
 	}
-
-	g := router.Group(prefix)
+	r := g.Group(prefix)
 	{
-		g.GET("/", gin.WrapF(pprof.Index))
-		g.GET("/cmdline", gin.WrapF(pprof.Cmdline))
-		g.GET("/profile", gin.WrapF(pprof.Profile))
-		g.POST("/symbol", gin.WrapF(pprof.Symbol))
-		g.GET("/symbol", gin.WrapF(pprof.Symbol))
-		g.GET("/trace", gin.WrapF(pprof.Trace))
-		g.GET("/allocs", gin.WrapH(pprof.Handler("allocs")))
-		g.GET("/block", gin.WrapH(pprof.Handler("block")))
-		g.GET("/goroutine", gin.WrapH(pprof.Handler("goroutine")))
-		g.GET("/heap", gin.WrapH(pprof.Handler("heap")))
-		g.GET("/mutex", gin.WrapH(pprof.Handler("mutex")))
-		g.GET("/threadcreate", gin.WrapH(pprof.Handler("threadcreate")))
+		r.GET("/", gin.WrapF(pprof.Index))
+		r.GET("/cmdline", gin.WrapF(pprof.Cmdline))
+		r.GET("/profile", gin.WrapF(pprof.Profile))
+		r.POST("/symbol", gin.WrapF(pprof.Symbol))
+		r.GET("/symbol", gin.WrapF(pprof.Symbol))
+		r.GET("/trace", gin.WrapF(pprof.Trace))
+		r.GET("/allocs", gin.WrapH(pprof.Handler("allocs")))
+		r.GET("/block", gin.WrapH(pprof.Handler("block")))
+		r.GET("/goroutine", gin.WrapH(pprof.Handler("goroutine")))
+		r.GET("/heap", gin.WrapH(pprof.Handler("heap")))
+		r.GET("/mutex", gin.WrapH(pprof.Handler("mutex")))
+		r.GET("/threadcreate", gin.WrapH(pprof.Handler("threadcreate")))
 	}
+	r.GET("/debug/vars", gin.WrapH(expvar.Handler()))
 }
